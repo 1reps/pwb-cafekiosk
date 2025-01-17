fb0f1d8a3cf38aef3f4ddef5cbb6feb54613fc7e
1reps <kdm9982@gmail.com>
2025-01-17 13:14:51 +0900

    ëRestDocs build

--- src/test/java/org/io/spring/docs/product/ProductControllerDocsTest.java
+++ src/test/java/org/io/spring/docs/product/ProductControllerDocsTest.java
@@ -7,6 +7,9 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -62,10 +65,13 @@ public class ProductControllerDocsTest extends RestDocsSupport {
             .andDo(print()) // print log
             .andExpect(status().isOk())
             .andDo(document("product-create",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
                 requestFields(
                     fieldWithPath("type").type(JsonFieldType.STRING)
                         .description("상품 타입"),
                     fieldWithPath("sellingStatus").type(JsonFieldType.STRING)
+                        .optional()
                         .description("상품 판매상태"),
                     fieldWithPath("name").type(JsonFieldType.STRING)
                         .description("상품 이름"),
